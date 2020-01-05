0ab263b1a74d1b2fef693ca350aad48cf20b7ea5
Silviu-Mihnea Cucuiet <Silviu.Cucuiet@msg.group>
2020-01-05 15:09:54 +0200

    added cross origin

--- src/main/java/com/uni10/backend/configuration/WebConfiguration.java
+++ src/main/java/com/uni10/backend/configuration/WebConfiguration.java
@@ -19,6 +19,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @AllArgsConstructor
@@ -74,5 +76,15 @@ public class WebConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public ObjectMapper objectMapper(){
         return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
