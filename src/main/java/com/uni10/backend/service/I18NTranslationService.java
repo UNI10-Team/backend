fdcb3cab254e3b047fa6c277fd48d3fc16d68076
Florin Buha <florinbuha15@yahoo.com>
2020-01-09 21:15:58 +0200

    Yo

--- src/main/java/com/uni10/backend/service/I18NTranslationService.java
+++ src/main/java/com/uni10/backend/service/I18NTranslationService.java
@@ -4,12 +4,11 @@ import com.uni10.backend.api.requests.I18NTranslationRequest;
 import com.uni10.backend.entity.I18NTranslation;
 import com.uni10.backend.repository.I18NTranslationRepository;
 import lombok.AllArgsConstructor;
+import lombok.val;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,5 +25,15 @@ public class I18NTranslationService {
                 .collect(Collectors.toMap(
                         i18NTranslation -> i18NTranslation.getResource().getName(),
                         I18NTranslation::getTranslation));
+    }
+
+    public String getTranslationMessage(final long resourceId, final String language) {
+        val optional = i18NTranslationRepository.findByResourceIdAndLanguage(resourceId, language);
+        if (optional.isPresent()) {
+            return optional.get().getTranslation();
+        } else {
+            val enOptional = i18NTranslationRepository.findByResourceIdAndLanguage(resourceId, language);
+            return enOptional.map(I18NTranslation::getTranslation).orElse("");
+        }
     }
 }
