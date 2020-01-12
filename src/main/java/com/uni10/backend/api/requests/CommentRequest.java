2721e8d1088c374783e4560f894b50b8b4d03424
uni10-team <uni10@scs.ubbcluj.ro>
2020-01-12 14:28:16 +0200

    Updated the CommentRequest class

--- src/main/java/com/uni10/backend/api/requests/CommentRequest.java
+++ src/main/java/com/uni10/backend/api/requests/CommentRequest.java
@@ -1,4 +1,26 @@
 package com.uni10.backend.api.requests;
 
-public class CommentRequest  extends PagedRequest{
+import com.uni10.backend.api.filter.Filter;
+import com.uni10.backend.entity.Comment;
+import com.uni10.backend.entity.Course;
+import com.uni10.backend.entity.Subject;
+import com.uni10.backend.specifications.Specifications;
+import io.swagger.annotations.ApiParam;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CommentRequest  extends PagedRequest implements Filter<Comment> {
+    @ApiParam(name = "attachmentId")
+    private List<String> attachmentId = new ArrayList<>();
+
+    @Override
+    public Specification<Comment> toSpecification() {
+        Set<Specification<Comment>> specifications = new HashSet<>();
+        specifications.add(toSpecification("attachment_id", attachmentId));
+        return Specifications.and(specifications);
+    }
 }
