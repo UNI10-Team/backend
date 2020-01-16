3bb7c093c896492dc9a056740615d4c4be4be8d0
UNI10-Team <57134578+UNI10-Team@users.noreply.github.com>
2020-01-16 16:11:24 +0200

    update user

--- src/main/java/com/uni10/backend/controller/UserController.java
+++ src/main/java/com/uni10/backend/controller/UserController.java
@@ -2,11 +2,13 @@ package com.uni10.backend.controller;
 
 import com.uni10.backend.api.dto.UserDTO;
 import com.uni10.backend.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -19,4 +21,15 @@ public class UserController {
     public ResponseEntity<UserDTO> getCurrentUser() {
         return ResponseEntity.ok(userService.getCurrentUser());
     }
+
+    @PutMapping("/{id}")
+    @ApiOperation(value = "UserController.update", notes = "Update an existent user")
+    public ResponseEntity<UserDTO> update(@Valid @RequestBody UserDTO userDTO,
+                                                  @PathVariable long id) {
+        //Long id1 = Long.parseLong(id);
+        return ResponseEntity.ok(userService.update(userDTO,id).get());
+    }
+
+
+
 }
